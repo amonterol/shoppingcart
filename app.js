88364b28cd3de905508fc4b5f4adf458d82311f4
amonterol <abmontero@yahoo.com>
2019-04-26 11:15:26 -0600

    fifth commit add product to cart and show cart

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
 var logger = require('morgan');
 var expressHbs = require('express-handlebars');
 var mongoose = require('mongoose');
@@ -9,7 +10,7 @@ var session = require('express-session');
 var passport = require('passport');
 var flash = require('connect-flash');
 var validator = require('express-validator');
-var bodyParser = require('body-parser');
+var MongoStore = require('connect-mongo')(session);
 
 var indexRouter = require('./routes/index');
 var userRoutes = require('./routes/user.js');
@@ -33,7 +34,13 @@ app.use(validator());
 //app.use(express.json());
 //app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(session({ secret: 'mysupersecret', resave: false, saveUninitialized: false }));
+app.use(session({ 
+  secret: 'mysupersecret', 
+  resave: false, 
+  saveUninitialized: false,
+  store: new MongoStore({ mongooseConnection: mongoose.connection }),
+  cookie: { maxAge: 180 * 60 * 1000}
+ }));
 app.use(flash());
 app.use(passport.initialize());
 app.use(passport.session());
@@ -41,6 +48,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(function(req, res, next) {
   res.locals.login = req.isAuthenticated();
+  res.locals.session = req.session;
   next();  
 });
 
