514453cfbf327198624ca9897f95393ec3af8ccd
amonterol <abmontero@yahoo.com>
2019-04-26 22:02:20 -0600

    sixth commit checkout and improvement shopping cart remove1 and remove all

--- routes/index.js
+++ routes/index.js
@@ -7,13 +7,14 @@ var Product = require('../models/product');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  var successMsg = req.flash( 'success' )[0];
   Product.find( function(err, docs) {
     var productChunks = [];
     var chunkSize = 3;
     for( var i = 0; i < docs.length; i += chunkSize) {
       productChunks.push( docs.slice(i, i + chunkSize));
     }
-    res.render('shop/index', { title: 'Shopping Cart!', products: productChunks });
+    res.render('shop/index', { title: 'Shopping Cart!', products: productChunks, successMsg: successMsg, noMessages: !successMsg });
   });
 });
 
@@ -32,12 +33,67 @@ router.get('/add-to-cart/:id', function(req, res, next) {
   });
 });
 
+router.get('/reduce/:id', function(req, res, next) {
+  var productId = req.params.id;
+  var cart = new Cart(req.session.cart ? req.session.cart: {});
+
+  cart.reduceByOne(productId);
+  req.session.cart = cart;
+  res.redirect('/shopping-cart');
+});
+
+router.get('/remove/:id', function(req, res, next) {
+  var productId = req.params.id;
+  var cart = new Cart(req.session.cart ? req.session.cart: {});
+
+  cart.removeItem(productId);
+  req.session.cart = cart;
+  res.redirect('/shopping-cart');
+});
+
+
 router.get('/shopping-cart', function(req, res, next) {
   if(!req.session.cart) {
     return res.render('shop/shopping-cart', {products: null});
   }
   var cart = new Cart(req.session.cart);
   res.render('shop/shopping-cart', {products: cart.generateArray(), totalPrice: cart.totalPrice});
+});
+
+router.get('/checkout', function(req, res, next) {
+  if(!req.session.cart) {
+    return res.redirect('/shopping-cart');
+  }
+  var cart = new Cart(req.session.cart);
+  var errMsg = req.flash('error')[0];
+  //algun tipo de validacion
+  res.render('shop/checkout', { total: cart.totalPrice, errMsg: errMsg, noError: !errMsg});
+});
+
+router.post('/checkout', function(req, res, next) {
+  if(!req.session.cart) {
+    return res.redirect('/shopping-cart');
+  }
+  var cart = new Cart(req.session.cart);
+  var stripe = require("stripe")(
+    "sk_test_SuiKA9MH9VSelfgNBknrTeLS00khzvu6cg"
+  );
+
+  stripe.charges.create({
+    //Nunca pedimos log in
+    amount: cart.totalPrice * 100,
+    currency: "usd",
+    source: req.body.stripeToken, // obtained with Stripe.js
+    description: "Test Charge"
+    }, function(err, charge) {
+      if (err) {
+        req.flash('error', err.message);
+        return res.redirect('/checkout');
+      }
+      req.flash('success', 'Successfully bought product');
+      req.session.cart = null;
+      res.redirect('/');
+  });
 });
 
 module.exports = router;
